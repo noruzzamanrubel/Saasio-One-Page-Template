9b4f7ffc05a8ef74c2c0895f71adc92f2cb0aacd
Noruzzaman Rubel <noruzzamanrubel@gmail.com>
2020-10-29 17:51:04 +0600

    Saasio Template

---
+++ assets/js/main.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+
+    $(window).on('scroll', function () {
+        if ( $(window).scrollTop() > 10 ) {
+            $('.navbar').addClass('active');
+        } else {
+            $('.navbar').removeClass('active');
+        }
+    });
+    
+    //Typed
+var options = {
+    strings: ['Problem.','Wants','Issue.'],
+    typeSpeed: 100,
+    backSpeed:100,
+    loop: true,
+};
+var typed = new Typed('#banner-typed', options);
+
+});
+
+
+//Counter 
+$(document).ready(function () {
+    $('.counter').countUp();
+});
+
+
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:20,
+        dots: false,
+        nav:true,
+        navText: ["<div class='fas fa-angle-left'></div>","<div class='fas fa-angle-right'></div>"],
+
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            768:{
+                items:2
+            },
+            1000:{
+                items:4
+            }
+        }
+    })
+});
+
+
+
